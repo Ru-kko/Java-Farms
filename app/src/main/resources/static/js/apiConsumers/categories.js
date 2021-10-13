8b484723474fb7ba2a25b188b5bd726a87433946
Ru-kko <verase2003.1@gmail.com>
2021-10-13 14:44:34 -0500

    front changes

--- app/src/main/resources/static/js/apiConsumers/categories.js
+++ app/src/main/resources/static/js/apiConsumers/categories.js
@@ -4,18 +4,18 @@
  *  @var 2 = Editing
  */
 const localState = 0;
-const domain = document.domain;
+const domain = document.domain == 'localhost' ? 'http://localhost:8080' : document.domain;
 
-$(document).ready(() =>{
+$(document).ready(() => {
     console.log(domain);
-    buld(); 
+    buld();
 })
-function buld(){
+function buld() {
     $('#categoriesRows').empty();
     $.ajax({
         type: 'GET',
-        url:  domain + '/api/Category' || 'http://localhost:8080/api/Category',
-        success: (res) =>{
+        url: domain + '/api/Category',
+        success: (res) => {
             res.forEach(i => {
                 const row = '<tr class="tableRow">' +
                     `<th scope="row" class="id">${i.id}</th>` +
@@ -29,11 +29,12 @@ function buld(){
     })
 }
 function edit() {
+    var used = false;
     const parent = $(this).parent().parent();
     var options = [
-        parent.find('th.id').text(),
-        parent.find('th.name').text(),
-        parent.find('th.description').text()
+        parent.find('th.id'),
+        parent.find('th.name'),
+        parent.find('th.description')
     ];
     var inputs = [
         $('#inputs div #idInput'),
@@ -42,17 +43,41 @@ function edit() {
     ];
 
     for (let i = 0; i < options.length; i++) {
-        inputs[i].val(options[i]);
-        console.log(inputs[i]);
+        inputs[i].val(options[i].text());
     };
 
-    inputs[0].css('display','flex');
+    inputs[0].css('display', 'flex');
 
-    $('#addCategory').css('display','none');
+    $('#addCategory').css('display', 'none');
 
+    $('#categoriesTable').css('display', 'none');
+    $('#inputContainer').css('display', 'block');
 
+    $('#uploadCategory').click(() => {
+        if(used) return;
+        $.ajax({
+            type: 'PUT',
+            url: domain + '/api/Category',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                'id': parseInt(inputs[0].val()),
+                'name': inputs[1].val(),
+                'description': inputs[2].val()
+            })
+        })      
+            .done(() => {
+                for (let i = 0; i < options.length; i++) {
+                    options[i].text(inputs[i].val());
+                };
+            })
+            .fail((e) => {
+                alert('An unspected error');
+                console.log(e);
+            })
+            .always(() => {
+                $('#categoriesTable').css('display', 'block');
+                $('#inputContainer').css('display', 'none');
+            })
 
-    $('#categoriesTable').css('display','none');
-    $('#inputContainer').css('display','block');
-
+    })
 }
