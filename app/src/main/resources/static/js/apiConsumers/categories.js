05932a4a1654822277fcdde2c2c1d86b0c43b659
Rukko <verase2003.1@gmail.com>
2021-10-13 15:46:51 -0500

    Front ui for category editor are finished

--- app/src/main/resources/static/js/apiConsumers/categories.js
+++ app/src/main/resources/static/js/apiConsumers/categories.js
@@ -6,12 +6,90 @@
 const localState = 0;
 const domain = document.domain == 'localhost' ? 'http://localhost:8080' : document.domain;
 
+var inputs = [
+    $('#inputs div #idInput'),
+    $('#inputs div #nameInput'),
+    $('#inputs div #descriptionInput')
+];
+
 $(document).ready(() => {
-    console.log(domain);
-    buld();
+    var used = false;
+
+    $('#sumbitCategory').click(add);
+
+    $('#removeCategory').click(() => {
+        if (used) return;
+        used = true;
+        $.ajax({
+            type: 'DELETE',
+            url: domain + '/api/Category',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                'id': parseInt(inputs[0].val()),
+            })
+        })
+            .fail(() => {
+                alert('An unspected error');
+            })
+            .always(() => {
+                used = false;
+                build();
+                $('#categoriesTable').css('display', 'block');
+                $('#inputContainer').css('display', 'none');
+            })
+    })
+
+    $('#uploadCategory').click(() => {
+        if (used) return;
+        used = true;
+        $.ajax({
+            type: 'PUT',
+            url: domain + '/api/Category',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                'id': parseInt(inputs[0].val()),
+                'name': inputs[1].val(),
+                'description': inputs[2].val()
+            })
+        })
+            .fail(() => {
+                alert('An unspected error');
+            })
+            .always(() => {
+                used = false;
+                build();
+                $('#categoriesTable').css('display', 'block');
+                $('#inputContainer').css('display', 'none');
+            })
+
+    });
+    $('#addCategory').click(() => {
+        if (used) return;
+        used = true;
+        $.ajax({
+            type: 'POST',
+            url: domain + '/api/Category',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                'name': inputs[1].val(),
+                'description': inputs[2].val()
+            })
+        })
+            .fail(() => {
+                alert('An unspected error');
+            })
+            .always(() => {
+                used = false;
+                build();
+                $('#categoriesTable').css('display', 'block');
+                $('#inputContainer').css('display', 'none');
+            })
+    });
+    build();
 })
-function buld() {
+function build() {
     $('#categoriesRows').empty();
+
     $.ajax({
         type: 'GET',
         url: domain + '/api/Category',
@@ -26,21 +104,30 @@ function buld() {
             });
             $('td a.btn-edit').click(edit);
         }
-    })
+    });
+}
+function add() {
+    $('#idInputG').css('display', 'none');
+
+    inputs.forEach(i => {
+        i.val('');
+    });
+
+    $('#addCategory').css('display', 'block');
+    $('#removeCategory').css('display', 'none');
+    $('#uploadCategory').css('display', 'none');
+
+    $('#categoriesTable').css('display', 'none');
+    $('#inputContainer').css('display', 'block');
 }
 function edit() {
-    var used = false;
-    const parent = $(this).parent().parent();
+    var parent = $(this).parent().parent();
     var options = [
         parent.find('th.id'),
         parent.find('th.name'),
         parent.find('th.description')
     ];
-    var inputs = [
-        $('#inputs div #idInput'),
-        $('#inputs div #nameInput'),
-        $('#inputs div #descriptionInput')
-    ];
+
 
     for (let i = 0; i < options.length; i++) {
         inputs[i].val(options[i].text());
@@ -49,35 +136,9 @@ function edit() {
     inputs[0].css('display', 'flex');
 
     $('#addCategory').css('display', 'none');
+    $('#removeCategory').css('display', 'block');
+    $('#uploadCategory').css('display', 'block');
 
     $('#categoriesTable').css('display', 'none');
     $('#inputContainer').css('display', 'block');
-
-    $('#uploadCategory').click(() => {
-        if(used) return;
-        $.ajax({
-            type: 'PUT',
-            url: domain + '/api/Category',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                'id': parseInt(inputs[0].val()),
-                'name': inputs[1].val(),
-                'description': inputs[2].val()
-            })
-        })      
-            .done(() => {
-                for (let i = 0; i < options.length; i++) {
-                    options[i].text(inputs[i].val());
-                };
-            })
-            .fail((e) => {
-                alert('An unspected error');
-                console.log(e);
-            })
-            .always(() => {
-                $('#categoriesTable').css('display', 'block');
-                $('#inputContainer').css('display', 'none');
-            })
-
-    })
 }
