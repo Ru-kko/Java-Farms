3814ff09bf7506a66e8bcaf01761bafc1c7f4947
Ru-kko <verase2003.1@gmail.com>
2021-10-15 20:09:05 -0500

    port listening massage was added

--- app/src/main/java/com/fincas/app/AppApplication.java
+++ app/src/main/java/com/fincas/app/AppApplication.java
@@ -1,15 +1,17 @@
 package com.fincas.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class AppApplication extends SpringBootServletInitializer{
-
+	@Value("${server.port}")
+	private static int serverPort;
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
-		System.out.println("\nListening in server port 8080\n");
+		System.out.println("\nListening in server port " + serverPort + "\n");
 	}
 
 }
